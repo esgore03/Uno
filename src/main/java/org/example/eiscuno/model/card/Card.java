7b67dfd40aebffc658115d828d9c0a9ae9049ed9
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-20 18:34:50 -0500

    Update Card.java

--- src/main/java/org/example/eiscuno/model/card/Card.java
+++ src/main/java/org/example/eiscuno/model/card/Card.java
@@ -3,9 +3,6 @@ package org.example.eiscuno.model.card;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-/**
- * Represents a card in the Uno game.
- */
 public class Card {
     private String url;
     private String value;
@@ -13,12 +10,6 @@ public class Card {
     private Image image;
     private ImageView cardImageView;
 
-    /**
-     * Constructs a Card with the specified image URL and name.
-     *
-     * @param url the URL of the card image
-     * @param value of the card
-     */
     public Card(String url, String value, String color) {
         this.url = url;
         this.value = value;
@@ -27,11 +18,6 @@ public class Card {
         this.cardImageView = createCardImageView();
     }
 
-    /**
-     * Creates and configures the ImageView for the card.
-     *
-     * @return the configured ImageView of the card
-     */
     private ImageView createCardImageView() {
         ImageView card = new ImageView(this.image);
         card.setY(16);
@@ -40,48 +26,27 @@ public class Card {
         return card;
     }
 
-    /**
-     * Gets the ImageView representation of the card.
-     *
-     * @return the ImageView of the card
-     */
     public ImageView getCard() {
         return cardImageView;
     }
 
-    /**
-     * Gets the image of the card.
-     *
-     * @return the Image of the card
-     */
     public Image getImage() {
         return image;
     }
 
-    /**
-     * Gets the value of the card.
-     *
-     * @return the value of the card as a string
-     */
     public String getValue() {
         return value;
     }
 
-    /**
-     * Gets the color of the card.
-     *
-     * @return the color of the card as a string
-     */
     public String getColor() {
         return color;
     }
 
-    /**
-     * Sets the color of the card.
-     *
-     * @param color the color to be set for the card
-     */
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public boolean isWildCard() {
+        return value.equalsIgnoreCase("WILD") || value.equalsIgnoreCase("WILD_DRAW_FOUR");
     }
 }
